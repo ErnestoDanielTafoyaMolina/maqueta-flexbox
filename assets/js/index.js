2f9e1af43728c687303e1775b08acb38bd4032eb
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2024-03-01 21:42:24 -0600

    lets add components

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
-console.log("Hola mundo!")
+class AppComponent extends HTMLElement {
+    constructor(){
+        super();
+        let shadow = this.attachShadow({ mode:'open' });
+        this.divHeader = document.createElement('div');
+        this.divHeader.innerHTML = `Hola mundo, soy un componente`;
+
+        shadow.appendChild(this.divHeader);
+    };
+    
+}
+
+
+customElements.define("app-component", AppComponent);
+const article1=document.querySelector("#article1");
+for(let i=1; i <= 2; i++){
+    
+    article1.innerHTML+=`<app-component />`;
+}
